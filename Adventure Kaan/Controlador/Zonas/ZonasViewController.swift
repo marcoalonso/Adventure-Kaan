a69c0204a74a5d086615e4fc6f4a012e11b8f06a
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-16 20:23:50 -0600

    Celda personalizada de zona e icon agregados

--- Adventure Kaan/Controlador/Zonas/ZonasViewController.swift
+++ Adventure Kaan/Controlador/Zonas/ZonasViewController.swift
@@ -13,8 +13,16 @@ class ZonasViewController: UIViewController {
         ZonasData(nombre: "Museo Maya", imagen: UIImage(named: "museomaya")!, latitud: 19.57744800, longitud: -88.045490, textoHistoria: "La que ahora es la cabecera del municipio Felipe Carrillo Puerto, en Quintana Roo, fue fundada el 15 de octubre de 1850 y se llamó, originalmente, en lengua maya Noj Kaaj Santa Cruz Xbáalam Naj, en español significa 'el gran pueblo Santa Cruz casa del jaguar', y así le dicen todavía los abuelos mayas. Este lugar fue considerado sagrado, es resultado del gran levantamiento maya, más conocido como la Guerra de Castas, que inició el 30 de julio de 1847. \n En esa conflagración peninsular inicialmente se enfrentaron blancos (tsulo’ob) contra mayas, a quienes luego se les unió gente de origen asiático, negros, mestizos e incluso blancos. El objetivo era acabar con el pago de las excesivas contribuciones y obvenciones eclesiásticas, contra la explotación que padecían y defender su territorio ante el crecimiento de las haciendas. Luego de iniciado el levantamiento, en poco tiempo se adueñó prácticamente de toda la península de Yucatán a excepción de las ciudades de Mérida y Campeche. Llegaron a las inmediaciones de la capital política y económica de la península, pero los mayas se retiraron, era tiempo de sembrar el santo maíz, nuestro principal alimento. El ejército yucateco pasó a la contraofensiva, los mayas decidieron replegarse y establecer su poder en esta región suroriental de la península.")
     ]
     
+    @IBOutlet weak var tablaZonas: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tablaZonas.register(UINib(nibName: "ZonaCelda", bundle: nil), forCellReuseIdentifier: "celda")
+        
+        tablaZonas.delegate = self
+        tablaZonas.dataSource = self
     
     }
     
@@ -27,6 +35,27 @@ class ZonasViewController: UIViewController {
         present(vc, animated: true)
     }
     
+    
+}
+
+extension ZonasViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        zonas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celda", for: indexPath) as! ZonaCelda
+        
+        celda.tituloZonaLabel.text = zonas[indexPath.row].nombre
+        celda.imagenZona.image = zonas[indexPath.row].imagen
+        
+        
+        return celda
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
+    }
     
 }
 
