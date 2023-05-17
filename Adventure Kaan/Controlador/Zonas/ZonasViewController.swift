586d94e6b0aca78c98ba3a2eec3f51023243a947
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-16 20:35:48 -0600

    Mostrando ubicación en mapa de cada zona

--- Adventure Kaan/Controlador/Zonas/ZonasViewController.swift
+++ Adventure Kaan/Controlador/Zonas/ZonasViewController.swift
@@ -6,13 +6,12 @@
 //
 
 import UIKit
+import MapKit
 
 class ZonasViewController: UIViewController {
     
-    var zonas : [ZonasData] = [
-        ZonasData(nombre: "Museo Maya", imagen: UIImage(named: "museomaya")!, latitud: 19.57744800, longitud: -88.045490, textoHistoria: "La que ahora es la cabecera del municipio Felipe Carrillo Puerto, en Quintana Roo, fue fundada el 15 de octubre de 1850 y se llamó, originalmente, en lengua maya Noj Kaaj Santa Cruz Xbáalam Naj, en español significa 'el gran pueblo Santa Cruz casa del jaguar', y así le dicen todavía los abuelos mayas. Este lugar fue considerado sagrado, es resultado del gran levantamiento maya, más conocido como la Guerra de Castas, que inició el 30 de julio de 1847. \n En esa conflagración peninsular inicialmente se enfrentaron blancos (tsulo’ob) contra mayas, a quienes luego se les unió gente de origen asiático, negros, mestizos e incluso blancos. El objetivo era acabar con el pago de las excesivas contribuciones y obvenciones eclesiásticas, contra la explotación que padecían y defender su territorio ante el crecimiento de las haciendas. Luego de iniciado el levantamiento, en poco tiempo se adueñó prácticamente de toda la península de Yucatán a excepción de las ciudades de Mérida y Campeche. Llegaron a las inmediaciones de la capital política y económica de la península, pero los mayas se retiraron, era tiempo de sembrar el santo maíz, nuestro principal alimento. El ejército yucateco pasó a la contraofensiva, los mayas decidieron replegarse y establecer su poder en esta región suroriental de la península.")
-    ]
     
+    @IBOutlet weak var mapaZonas: MKMapView!
     @IBOutlet weak var tablaZonas: UITableView!
     
     
@@ -21,9 +20,26 @@ class ZonasViewController: UIViewController {
         
         tablaZonas.register(UINib(nibName: "ZonaCelda", bundle: nil), forCellReuseIdentifier: "celda")
         
+        mapaZonas.delegate = self
+        
         tablaZonas.delegate = self
         tablaZonas.dataSource = self
+        
+        showAnnotations()
     
+    }
+    
+    private func showAnnotations(){
+        
+        for lugar in zonas {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: lugar.latitud, longitude: lugar.longitud)
+            annotation.title = lugar.nombre
+            
+            mapaZonas.addAnnotation(annotation)
+        }
+        
+
     }
     
     
@@ -35,6 +51,31 @@ class ZonasViewController: UIViewController {
         present(vc, animated: true)
     }
     
+    
+}
+
+extension ZonasViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // Personaliza el aspecto del marcador en función de tu preferencia
+        // Puedes utilizar MKMarkerAnnotationView, MKPinAnnotationView u otros
+        
+        // Ejemplo utilizando MKMarkerAnnotationView:
+        let identifier = "CustomAnnotation"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        // Personaliza la apariencia del marcador
+        annotationView?.markerTintColor = UIColor.red
+//        annotationView?.glyphText = "1"
+        
+        return annotationView
+    }
     
 }
 
