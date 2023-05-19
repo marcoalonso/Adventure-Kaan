fec40be1cb1ea184a84d4577efd038c515285261
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-18 22:19:03 -0600

    Mostrando Pagina Web de Adventure Ka'an para poder subirla a AppStore

--- Adventure Kaan/Controlador/AboutUsViewController.swift
+++ Adventure Kaan/Controlador/AboutUsViewController.swift
@@ -6,29 +6,33 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutUsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var webPage: WKWebView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loadWebPage()
+        
     }
     
+    private func loadWebPage(){
+        if let url = URL(string: "https://adventure-kaan.webnode.mx/") {
+            let request = URLRequest(url: url)
+            webPage.load(request)
+        }
+    }
     
     @IBAction func backButton(_ sender: UIButton) {
         dismiss(animated: true)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
